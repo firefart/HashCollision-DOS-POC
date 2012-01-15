28d607fb4c011a1d24583d3ec570817ace1381b1
Christian Mehlmauer <FireFart@gmail.com>
2012-01-15 22:23:38 +0100

    Wrong extension in sample

--- HashtablePOC.py
+++ HashtablePOC.py
@@ -26,7 +26,7 @@ python HashtablePOC.py -u https://host/index.php -v -c 1 -w -o output -t PHP
 python HashtablePOC.py -u https://host/index.php -v -c 500 -t PHP
 
 -) Take down a JAVA server(make 500 requests without waiting for a response, maximum POST data size 2MB):
-python HashtablePOC.py -u https://host/index.php -v -c 500 -t JAVA -m 2
+python HashtablePOC.py -u https://host/index.jsp -v -c 500 -t JAVA -m 2
 
 Changelog:
 v6.0: Added Javapayloadgenerator
